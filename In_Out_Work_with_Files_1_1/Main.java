23784829acc4b4de5af39af8b99b217f67065f78
Vladimir Ivanov <ivasin75@mail.ru>
2020-09-21 19:23:54 +0300

    all files

---
+++ In_Out_Work_with_Files_1_1/Main.java
@@ -0,0 +1,61 @@
+package In_Out_Work_with_Files_1_1;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        File dirGames = new File("Games");
+        if (dirGames.mkdir()) stringBuilder.append("Директория "+ dirGames + " создана\n");
+        File dirGamesTemp = new File("Games\\temp");
+        if (dirGamesTemp.mkdir()) stringBuilder.append("Директория "+ dirGamesTemp + " создана\n");
+        File temp = new File("Games\\temp\\temp.txt");
+        try {if (temp.createNewFile()) stringBuilder.append("Файл " + temp + " создан\n");}
+        catch (IOException ex) {stringBuilder.append("Файл " + temp + " уже создан\n");}
+        File dirSaveGames = new File("Games\\savegames");
+        if (dirSaveGames.mkdir()) stringBuilder.append("Директория "+ dirSaveGames + " создана\n");
+
+        File dirScr = new File("Games\\scr");
+        if (dirScr.mkdir()) stringBuilder.append("Директория "+ dirScr + " создана\n");
+        File dirSrcMain = new File("Games\\scr\\main");
+        if (dirSrcMain.mkdir()) stringBuilder.append("Директория "+ dirSrcMain + " создана\n");
+
+        File main = new File("Games\\scr\\main\\Main.java");
+        try {if (main.createNewFile()) stringBuilder.append("Файл " + main + " создан\n");}
+        catch (IOException ex) {System.out.println(ex.getMessage());}
+        File utils = new File("Games\\scr\\main\\Utils.java");
+        try {if (utils.createNewFile()) stringBuilder.append("Файл " + utils + " создан\n");}
+        catch (IOException ex) {System.out.println(ex.getMessage());}
+
+
+        File dirSrcTest = new File("Games\\scr\\test");
+        if (dirSrcTest.mkdir()) stringBuilder.append("Директория "+ dirSrcTest + " создана\n");
+
+        File dirRes = new File("Games\\res");
+        if (dirRes.mkdir()) stringBuilder.append("Директория "+ dirRes + " создана\n");
+        File dirResDrawables = new File("Games\\res\\drawables");
+        if (dirResDrawables.mkdir()) stringBuilder.append("Директория "+ dirResDrawables + " создана\n");
+        File dirResVectors = new File("Games\\res\\vectors");
+        if (dirResVectors.mkdir()) stringBuilder.append("Директория "+ dirResVectors + " создана\n");
+        File dirResIcons = new File("Games\\res\\icons");
+        if (dirResIcons.mkdir()) stringBuilder.append("Директория "+ dirResIcons + " создана\n");
+
+        try(FileWriter writer = new FileWriter(temp, false))
+        {
+            // запись всего лога
+            writer.write(String.valueOf(stringBuilder));
+
+
+            writer.flush();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ In_Out_Work_with_Files_Saving_1_2/Main.java
@@ -0,0 +1,65 @@
+package In_Out_Work_with_Files_Saving_1_2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> path = new ArrayList<>();
+        GameProgress game1 = new GameProgress(98, 37, 15, 125.32);
+        String gameAddress = "Games\\savegames\\save1.dat";
+        path.add(savegame(gameAddress, game1));
+        GameProgress game2 = new GameProgress(54, 23, 21, 103.29);
+        gameAddress = "Games\\savegames\\save2.dat";
+        path.add(savegame(gameAddress, game2));
+        GameProgress game3 = new GameProgress(23, 15, 45, 438.18);
+        gameAddress = "Games\\savegames\\save3.dat";
+        path.add(savegame(gameAddress, game3));
+        String zipPath = "Games\\savegames\\save.zip";
+        zipFiles(zipPath, path);
+
+        File dir = new File("Games\\savegames");
+        if (dir.isDirectory()){
+        for (File file: dir.listFiles()){
+            if (!file.getName().equals("save.zip"))
+            file.delete();
+            }
+        }
+    }
+
+
+    public static String savegame(String strPath, GameProgress gameProgress) {
+        try (FileOutputStream fos = new FileOutputStream(strPath);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+        } catch (Exception ex) {
+            System.out.println((ex.getMessage()));
+        }
+        return strPath;
+    }
+
+    public static void zipFiles(String zipPath, List<String> path) {
+        try (ZipOutputStream zout = new ZipOutputStream(//создаем архив
+                new FileOutputStream(zipPath))) {  // и указываем в какой файл будет производиться запись
+            for (String s : path) {
+                int i = s.lastIndexOf("save");
+                String nameFile = s.substring(i);
+                try (FileInputStream fis = new FileInputStream(s)) {//считываем файл, который хотим поместить в архив
+                    ZipEntry entry = new ZipEntry(nameFile);//создаем объект, в котором будет храниться архивируемый файл
+                    zout.putNextEntry((entry));//помещаем этот объект в архив zout
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);//считываем содержимое файла s
+                    zout.write(buffer);
+                    zout.closeEntry();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
